ae4df342a651e65d3df24c89dcedf0fee097b828
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-02 17:53:20 +0100

    create basic layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu")
+
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active")
+
+    // add eventlistener to menu to remove it
+})
