5b70398df556230dde3f6aa108461640a1dd7150
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-04 00:00:50 +0100

    fix typo

--- src/one.js
+++ src/one.js
@@ -2,7 +2,7 @@ export default function createOne(){
     const content = document.querySelector(".content")
     const text1 = document.createElement("p")
     text1.className = "text"
-    text1.textContent = "Try our napolean pizza"
+    text1.textContent = "Try our neopolitan pizza"
     content.appendChild(text1)
     const image1 = document.createElement("img")
     image1.className = "image"
