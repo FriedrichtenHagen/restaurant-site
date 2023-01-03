64873fcd1f2f813022f502987831f430d0855cb8
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-03 20:41:04 +0100

    setup webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.querySelector(".hamburger"),t=document.querySelector(".menu"),c=document.querySelector(".content");e.addEventListener("click",(()=>{t.classList.add("active"),alert("asd")}));const a=document.createElement("img");a.className="image",a.src="pizza1.jpg",c.appendChild(a)})();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const hamburger = document.querySelector(".hamburger")
 const menu = document.querySelector(".menu")
+const content = document.querySelector(".content")
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active")
-
+alert("asd")
     // add eventlistener to menu to remove it
 })
+
+
+    const image1 = document.createElement("img")
+    image1.className = "image"
+    image1.src = "pizza1.jpg"
+    content.appendChild(image1)
